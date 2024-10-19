37a26791a3bf8581007ce5980ce9849b1d334472
Abbosjon0 <abbosjonanvarov001@gmail.com>
2024-10-19 18:49:30 +0500

    main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ teacher=pd.DataFrame({
     'Familya': ['Xalilov','Toxirova'],
     'Fan': ['Suniy Intilek','Suniy Intilek'],
     'Telefon raqami': ['+998991234567','+998991234567']
-    
+    'Ish joyi': ['TATU FF','TATU FF']
 })
 print(teacher)
 teacher.to_excel('teacher.xlsx',index=False,sheet_name='Sheet1')
